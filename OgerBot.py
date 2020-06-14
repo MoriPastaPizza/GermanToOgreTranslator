d4f069aa1dbcdbf44c8127dbd1eac6e8a050f571
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-06-14 11:30:07 +0200

    Rebase

---
+++ OgerBot.py
@@ -0,0 +1,199 @@
+import os
+import time
+import json
+import random
+import threading
+import re
+
+import praw
+import yaml
+from prawcore import NotFound
+
+
+class OgreBot(threading.Thread):
+    __config = dict()
+    __dictionary = dict()
+    __error_dictionary = dict()
+    __header_dictionary = dict()
+    __active_bots = list()
+    __bot_name = str
+    __reddit = praw.Reddit
+    __is_cancellation_requested = False
+    __test_mode = bool
+
+    # Constructor of the "Ogre_Bot" Jesus....
+    def __init__(self, bot_name, bot_user_agent, test_mode=False):
+
+        if bot_name in self.__active_bots:
+            raise Exception("This bot already Exists")
+
+        self.__test_mode = test_mode
+        self.__config = self.__init_config_instance()
+        self.__dictionary = self.__init_dictionary("dict/dictionary_default.json")
+        self.__error_dictionary = self.__init_dictionary("dict/dictionary_error_messages.json")
+        self.__header_dictionary = self.__init_dictionary("dict/dictionary_header.json")
+        self.__reddit = self.__init_reddit_instance(bot_name, bot_user_agent)
+        self.__bot_name = bot_name
+        self.__active_bots.append(bot_name)
+
+        # Constructor of super class (Threading)
+        threading.Thread.__init__(self)
+        print("Created Bot: " + self.__bot_name)
+
+    # Deconstructer
+    def __del__(self):
+        self.__active_bots.remove(self.__bot_name)
+        print("Deleted Bot: " + self.__bot_name)
+
+    # Override of super class starts the Thread
+    def start(self) -> None:
+        self.__is_cancellation_requested = False
+        super().start()
+        print("Started Bot: " + self.__bot_name)
+
+    # stop current Bot
+    def stop(self):
+        self.__is_cancellation_requested = True
+        print("Stopped Bot: " + self.__bot_name)
+
+    # Override of super class for Run Function => Threading
+    def run(self):
+
+        while not self.__is_cancellation_requested:
+
+            try:
+
+                time.sleep(self.__config['BOT_TIMEOUT_MS'] / 1000)
+
+                # Search mentions in inbox
+                inbox = list(self.__reddit.inbox.unread(limit=self.__config['BOT_INBOX_LIMIT']))
+                inbox.reverse()
+                for message in inbox:
+                    try:
+                        if self.__check_if_valid_message(message):
+                            self.__process_message(message)
+                        else:
+                            message.mark_read()
+                    except Exception as e:
+                        print(e)
+                        self.__send_error_message(message)
+
+            except NotFound:
+                pass
+            except Exception as e:
+                print(e)
+
+    def __process_message(self, message):
+
+        try:
+
+            if "/u/" + self.__bot_name + " \\--" in message.body:
+
+                additional_parameter = re.search("--(.*)", message.body)
+                additional_parameter = additional_parameter.group(1).split(" ")[0]
+
+                # Switch case maybe?
+                if additional_parameter == 'error':
+                    self.__send_error_message(message)
+                    return
+
+            translated_comment = self.__translate_to_ogre_speak(message.parent().body)
+            reply = self.__config['HEADER'] + random.choice(self.__header_dictionary['header']) + translated_comment + \
+                self.__config['FOOTER']
+
+            if not self.__test_mode:
+                message.reply(reply)
+                message.mark_read()
+
+            print("New Comment!!!"
+                  + "\n\tFrom: " + str(message.author)
+                  + "\n\tOriginal: " + message.parent().body
+                  + "\n\tTranslation: " + translated_comment
+                  + "\n\t")
+
+        except Exception as e:
+            print(e)
+            self.__send_error_message(message)
+            return
+
+    def __translate_to_ogre_speak(self, text_to_translate):
+
+        full_list = self.__dictionary['translate']
+
+        # NO!!! you can't just go through a whole Json Dictionary and check everything
+        # hahah for loop goes: brrrrrrrrrrrrrrrrrrrrrrrr
+        for key, item in full_list.items():
+            word_to_translate = key
+            if len(item) > 1:
+                translated_word = random.choice(item)
+            else:
+                translated_word = item[0]
+            text_to_translate = text_to_translate.replace(word_to_translate, translated_word)
+
+        return text_to_translate
+
+    def __check_if_valid_message(self, message):
+        if (message.type == 'username_mention' or '/u/' + self.__bot_name in message.body) \
+                and isinstance(message, praw.reddit.Comment):
+            return True
+        return False
+
+    def __send_error_message(self, message):
+
+        try:
+            error_message = random.choice(self.__error_dictionary['error'])
+            print(error_message)
+
+            reply = self.__config['HEADER_ERROR'] + error_message + self.__config['FOOTER_ERROR']
+
+            if not self.__test_mode:
+                message.reply(reply)
+                message.mark_read()
+
+        except Exception as e:
+            print(e)
+            return
+
+    @staticmethod
+    def __init_reddit_instance(bot_name, bot_user_agent):
+        return praw.Reddit(bot_name, user_agent=bot_user_agent)
+
+    @staticmethod
+    def __init_config_instance():
+        conf_file = os.path.relpath("cfg/config.yaml")
+        with open(conf_file, encoding='utf8') as f:
+            configuration = yaml.safe_load(f)
+
+        configuration['FOOTER'] = "\n\n ***  \n" + configuration['INFO_LINK']\
+            + "&#32;|&#32;" + "&#32;|&#32;" + configuration['GITHUB_LINK']
+
+        configuration['FOOTER_ERROR'] = "\n\n ***  \n" + configuration['INFO_TEXT_ERROR'] + "\n"\
+            + configuration['INFO_LINK'] + "&#32;|&#32;" + "&#32;|&#32;"\
+            + configuration['GITHUB_LINK']
+
+        return configuration
+
+    @staticmethod
+    def __init_dictionary(path):
+        json_path = os.path.relpath(path)
+        with open(json_path, encoding='utf-8') as json_file:
+            return json.load(json_file)
+
+
+def startup():
+    try:
+        my_bot = OgreBot("DeutschZuOgerBot", "PastaPizzaSecretAgent", test_mode=False)
+        my_bot.start()
+        input("Press enter to exit")
+        my_bot.stop()
+        del my_bot
+    except Exception as e:
+        print(e)
+        print("Restarting...")
+        del my_bot
+        startup()
+
+
+# init and run routine
+if __name__ == '__main__':
+    startup()
---
+++ README.md
@@ -0,0 +1,88 @@
+
+
+<h1 align=center>Deutsch zu Oger / MeddlFränkisch Übersetzer</h1>
+<p align=center>Ein Reddit-Bot welcher deutsche Sätze in "Ogersprache" übersetzt</p>
+
+<p align=center>
+	<img src="images/ruinerbot.png" alt="Logo" width="45%" height="45%">
+</p>
+
+## Inhalt
+
+* [Über](#über)
+* [How-To](#how-to)
+* [Fehlermeldungen](#fehlermeldungen)
+	* [Fehlermeldung erzwingen](#fehlermeldung-erzwingen)
+* [Du willst mithelfen? Oder das Wörterbuch erweitern?](#du-willst-mithelfen-oder-das-wörterbuch-erweitern)
+	* [Wörterbuch erweitern](#wörterbuch-erweitern)
+	* [Normale Menschen](#du-kennst-dich-etzadla-mitm-bezeh-net-so-gut-aus)
+	* [Nerds](#du-hast-scho-amoal-in-gimb-was-neigspeicherd)
+* [Selber einen Bot bauen](#selber-einen-bot-bauen)
+* [Danksagungen](#danksagung)
+
+## Über
+Inspiriert von diesem [Post](https://www.reddit.com/r/Drachenlord/comments/gyiaiv/es_muss_gesagt_werden/) .
+
+Da einige Posts im [Drachenlord Subreddit](https://www.reddit.com/r/Drachenlord/) in "normalen" Hochdeutsch verfasst werden, oder es einfach zu viel hadde Abbeid ist einen langen Text ins Meddlfrängische zu übersetzten, wurde dieser Bot erschaffen.
+
+Obwohl der Wortschatz von diesem Bot einen ziemlich hohen Ikuh hat, hat er doch noch viel Verbesserungsbedarf. Und **DU** kannst dabei helfen, schau einfach [hier](#du-willst-mithelfen-oder-das-wörterbuch-erweitern)!
+
+Das Drachengame hat mir viele Lacher und Momente beschert. Ich wollte auch mal meinen kleinen Beitrag leisten. Ich hoffe dieser Bot wird viel benutzt, bringt euch ein paar Lacher und wird von vielen Wahnsinnichen [verbessert](#du-willst-mithelfen-oder-das-wörterbuch-erweitern).
+
+> PS: Dieser Bot funktioniert in allen Subreddits, also meddelt den auch in anderen deutschprachigen Subs raus!
+
+## How-To
+Dich nervt ein Post / Kommentar der im "Hochdeutschen" geschrieben wurde? Erwähne einfach den [DeutschZuOgerBot](https://www.reddit.com/user/DeutschZuOgerBot/) in einem Kommentar!
+
+    /u/DeutschZuOgerBot
+   
+   Er sollte so schnell wie möglich zurückmeddln und den orginal Text übersetzten!
+
+![Beispiel](https://github.com/MoriPastaPizza/GermanToOgreTranslator/blob/master/images/screenshot.jpg?raw=true)
+
+## Fehlermeldungen
+
+Ab und an kann es zu Fehlern kommen, aber nicht wegen mir.
+
+> Ich bin das höchste Wesne, ich mach keine Fehler. Des waren wieder die Gagg Haider die schon bei GidHub abeiden und meinen Code manubulieren!!
+
+*SOLLTE* es zu Fehlern kommen meddelt sich der bot mit einer zufälligen Fehlermeddlung:
+
+![Fehlermeddlung](https://github.com/MoriPastaPizza/GermanToOgreTranslator/blob/master/images/screenshot_error.jpg?raw=true)
+
+#### Fehlermeldung erzwingen
+
+Um eine Fehlermeldung zu erzwingen musst du einfach den Parameter "--error" hinter den Aufruf schreiben:
+
+    /u/DeutschZuOgerBot --error
+  
+## Du willst mithelfen? Oder das Wörterbuch erweitern?
+### Wörterbuch erweitern
+Im [dict](https://github.com/MoriPastaPizza/GermanToOgreTranslator/tree/master/dict "dict") Ordner sind 3 Dateien
+
+ 1. [dictionary_default.json](https://github.com/MoriPastaPizza/GermanToOgreTranslator/blob/master/dict/dictionary_default.json "dictionary_default.json")
+ 2. [dictionary_error_messages.json](https://github.com/MoriPastaPizza/GermanToOgreTranslator/blob/master/dict/dictionary_error_messages.json "dictionary_error_messages.json")
+ 3. [dictionary_header.json](https://github.com/MoriPastaPizza/GermanToOgreTranslator/blob/master/dict/dictionary_header.json "dictionary_header.json")
+ 
+ Die erste ist das eigentlich Wörterbuch.  
+ Das zweite ist für die zufälligen Fehlermeldungen.  
+ Das dritte ist für die zufälligen Header beim Posten (hauptsächlich Zitate vom [Oger](https://github.com/MoriPastaPizza/GermanToOgreTranslator/blob/master/images/ruiner.jpeg)).
+ 
+  Die Wörter vor dem : sind die Wörter nach welchen gesucht werden soll. Die Wörter in den eckigen Klammern [] sind die, welche die gefundenen ersetzten.... Sollten mehrere Wörter in den Klammern stehen, wird eins zufällig davon ausgewählt.
+#### Du kennst dich etzadla mitm Bezeh net so gut aus?
+Kein Problem! Schick [mir](https://www.reddit.com/user/MoriPastaPizza) einfach deine Wörterbuch Vorschläge so! Egal ob für das eigentliche Wörterbuch oder die Fehlermeddlungen oder die Header! Ich versuche diese dann mit einzubinden.
+
+#### Du hast scho amoal in Gimb was neigspeicherd?
+Du kannst mir gerne helfen diesen Bot zu verbessern! Einfach einen Pullrequest machen!
+Jeder Vorschlag / Verbesserung am Source-Code oder an den Dictionaries ist herzlich wilkommen.
+
+ 
+
+## Selber einen Bot bauen
+
+## Danksagung
+
+* [VredditDownloader](https://github.com/JohannesPertl/vreddit-download-bot)
+* [Altschauerbergwiki](https://altschauerberg.com/index.php/Winklersche_Rechtschreibreform)
+* [Praw](https://praw.readthedocs.io/en/latest)
+* [Readme Template](https://github.com/othneildrew/Best-README-Template/blob/master/README.md)
